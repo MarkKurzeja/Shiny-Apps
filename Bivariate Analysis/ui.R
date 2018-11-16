b33859e26aac51f0f82c8227660c1adde712a7da
MarkKurzeja <mtkurzej@umich.edu>
2018-11-16 00:17:20 -0500

    Added Binary Plot Graph

--- Bivariate Analysis/ui.R
+++ Bivariate Analysis/ui.R
@@ -181,6 +181,149 @@ shinyUI(
       # End of Bivariate Tab
       ################################################################################
       #                                                                              #
+      #                                Binary Plot Tab                               #
+      #                                                                              #
+      ################################################################################
+      tabPanel(
+        "Binary Regression",
+        titlePanel("Binary Regression"),
+        wellPanel(
+          fluidRow(
+            column(
+              12,
+              fluidRow(
+                column(
+                  12,
+                  tags$h4("Upload a dataset or choose a dataset")
+                )
+              ),
+              column(
+                7,
+                fluidRow(
+                  column(
+                    7,
+                    tags$h5("Choose a Default Dataset")
+                  )
+                ),
+                fluidRow(
+                  column(
+                    7,
+                    column(
+                      6,
+                      selectInput(
+                        "defaultDataSelection",
+                        label = NULL,
+                        choices = c(
+                          "Wt | MPG",
+                          "Bivar. Normal"
+                        )
+                      )
+                    ),
+                    column(
+                      1,
+                      actionButton(
+                        "defaultData",
+                        "Use Default Dataset",
+                        width = 175
+                      )
+                    )
+                  )
+                ),
+                fluidRow(
+                  column(
+                    7,
+                    tags$h5("Or Upload your Own CSV")
+                  )
+                ),
+                fluidRow(
+                  column(
+                    7,
+                    column(
+                      6,
+                      fileInput(
+                        "file1",
+                        label = NULL,
+                        multiple = TRUE,
+                        accept = c("text/csv",
+                                   "text/comma-separated-values,text/plain",
+                                   ".csv")
+                      )
+                    ),
+                    column(
+                      1,
+                      actionButton(
+                        "uploadedData",
+                        "Use Uploaded Dataset",
+                        width = 175
+                      )
+                    )
+                  )
+                )
+              ),
+              column(
+                2,
+                tags$h4("Plotting Options"),
+                checkboxGroupInput(
+                  "dispOptions",
+                  label = NULL,
+                  choices = c("Display Mean", "Display Fit")
+                )
+              ),
+              column(
+                3,
+                tags$h4("Prompt Display Options"),
+                selectInput(
+                  "whichPrompt",
+                  label = NULL,
+                  choices = c(
+                    "Discussion",
+                    "Correlation",
+                    "Diagnostics - 1",
+                    "Diagnostics - 2",
+                    "Diagnostics - 3",
+                    "None"
+                  ),
+                  selected = "None"
+                )
+                # tags$h4("Correlation Slider"),
+                # sliderInput(
+                #   "corrSlider",
+                #   label = NULL,
+                #   min = -1, 
+                #   max = 1, 
+                #   value = 0, 
+                #   step = 0.10
+                # )
+              )
+            )
+          )
+        ),
+        ################################# Prompt Line ##################################
+        fluidRow(
+          column(
+            10,
+            withMathJax(htmlOutput("bivarprompt"))
+          )
+        ),
+        ######################### Main Panel of Bivariate Plot #########################
+        fluidRow(
+          column(
+            3,
+            h4("Distribution of Responses"),
+            plotOutput("yMarginal", height = 350)
+          ),
+          column(
+            9,
+            h4("Response Vs. X"),
+            plotOutput("mainPlot", height = 350, click = "plot_click"),
+            h4("Distribution of X"),
+            plotOutput("xMarginal", height = 150)
+          )
+        )
+      ),
+      # End of binary tab
+      ################################################################################
+      #                                                                              #
       #                                Demo Dataset                                  #
       #                                                                              #
       ################################################################################
